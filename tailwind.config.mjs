b4b18f97071ae1c68ee0d03e89e030d7146b928a
renanromagnollo <renanromagnoli@yahoo.com.br>
2024-07-04 09:30:38 -0300

    feat: basic tailwind added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF2E63",
+        secondary: "#08D906",
+        dark: "#252A34",
+        light: "#EAEAEA",
+      },
+    },
+    fontFamily: {
+      sans: ["Montserrat", "ui-sans-serif", "system-ui"],
+    },
+  },
+  plugins: [],
+};
